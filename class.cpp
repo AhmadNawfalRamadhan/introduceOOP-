fe76aaaee86fe18cb081da58e1bdae7672078e53
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-04-28 10:10:15 +0700

    class contoh

---
+++ class.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+        int nim;
+        string nama;
+        float nilai;
+
+        void printData(){
+            cout << "NIM = " << nim << endl;
+            cout << "Nama = " << nama << endl;
+            cout << "Nilai = " << nilai << endl;
+        }
+};
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = 2024;
+    mhs.nama = "sule";
+    mhs.nilai = 75,5;
+
+    mhs.printData();
+    
+}
